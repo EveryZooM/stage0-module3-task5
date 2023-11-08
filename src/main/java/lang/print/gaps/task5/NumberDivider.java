a98b8124f7143d3a138ba39675f5461b3af0e662
ZooM <yuhos@mail.ru>
2023-11-08 23:24:30 +0300

    Work with various arithmetic operators

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide(float toBeDivided) {
+        float result = toBeDivided % 5 + (((toBeDivided % 5) / 10) * 2 - 1);
+        System.out.println(result);
     }
 }
