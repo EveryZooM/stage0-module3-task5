e89724d8b6627100cb20da582a17ea29819665f9
ZooM <yuhos@mail.ru>
2023-11-10 01:05:11 +0300

    Reduced code complexity

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,8 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(float toBeDivided) {
-        float result = toBeDivided % 5 + (((toBeDivided % 5) / 10) * 2 - 1);
+    void divide(double toBeDivided) {
+        double result = toBeDivided / 5;
         System.out.println(result);
     }
 }
